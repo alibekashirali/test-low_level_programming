0e02eb7d0ffc5e9cf9480e518fc1026c61959b81
alibekashirali <ashiralialibek@gmail.com>
2025-03-03 18:05:31 +0000

    asd

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,9 +4,12 @@
  * _islower - Checks if a character is lowercase
  * @c: The character to check
  *
- * Return: 1 if lowercase, 0 otherwise
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-    return (c >= 'a' && c <= 'z');
+    if (c >= 'a' && c <= 'z')
+        return (1);
+    else
+        return (0);
 }
