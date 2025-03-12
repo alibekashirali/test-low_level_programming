b59e895a684899529e11d57d1d35cdb9e2c3762a
alibekashirali <ashiralialibek@gmail.com>
2025-03-12 18:36:23 +0000

    asdasdasd

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,17 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * _puts - Prints a string followed by a new line
  * @str: String to print
  */
+
 void _puts(char *str)
 {
-    while (*str != '\0')
+    while (*str)
     {
-        putchar(*str);
+        write(1, str, 1);
         str++;
     }
-    putchar('\n');
+    write(1, "\n", 1);
 }
-
