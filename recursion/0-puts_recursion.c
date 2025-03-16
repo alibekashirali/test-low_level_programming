ff372ce5de5dfc9806eae39f3db988102227d24e
alibekashirali <ashiralialibek@gmail.com>
2025-03-16 14:20:52 +0000

    asdas

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,15 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes a character to stdout
- * @c: The character to print
- * Return: On success 1, on error -1.
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
 
 /**
  * _puts_recursion - Prints a string followed by a new line
